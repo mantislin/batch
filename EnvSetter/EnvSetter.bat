6bcf11a5582d91bbe13736fc5d07cd1debc41a1f
mantislin <mntslin@gmail.com>
2014-09-13 21:07:47 +0800

    update get admin method

--- EnvSetter/EnvSetter.bat
+++ EnvSetter/EnvSetter.bat
@@ -12,18 +12,34 @@ if '%errorlevel%' NEQ '0' (
 ) else ( goto :gotAdmin )
 
 :UACPrompt
-    setlocal enabledelayedexpansion
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    SETLOCAL ENABLEDELAYEDEXPANSION
+    set hour=%time:~0,2%
+    if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+    set min=%time:~3,2%
+    if "%min:~0,1%" == " " set min=0%min:~1,1%
+    set sec=%time:~6,2%
+    if "%sec:~0,1%" == " " set sec=0%sec:~1,1%
+    set msec=%time:~9,2%
+    if "%msec:~0,1%" == " " set msec=0%msec:~1,1%
+
+    set year=%date:~-4%
+    set month=%date:~3,2%
+    if "%month:~0,1%" == " " set month=0%month:~1,1%
+    set day=%date:~0,2%
+    if "%day:~0,1%" == " " set day=0%day:~1,1%
+
+    set "vbsname=getadmin_%year%%month%%day%_%hour%%min%%sec%%msec%.vbs"
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\%vbsname%"
     set params=%*
     if "%params%" NEQ "" (
         set params=%params:"=""%
         set "params= !params!"
     )
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0%params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    endlocal
-    exit/b
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0%params%", "", "runas", 1 >> "%temp%\%vbsname%"
+    "%temp%\%vbsname%"
+    rem del "%temp%\%vbsname%"
+    ENDLOCAL
+    exit /B
 
 :gotAdmin
     pushd "%CD%"
