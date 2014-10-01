912f7fe65b17f3e029e23c22140edfd48cec2adc
mantislin <mntslin@gmail.com>
2014-10-01 17:36:28 +0800

    change some coding format

--- RegSetter/REG_shutdownwithoutlogon.bat
+++ RegSetter/REG_shutdownwithoutlogon.bat
@@ -1,15 +1,10 @@
-@IF "%~1" EQU "" (
-@    GOTO:EOC
+@if "%~1" equ "" (
+@    goto :eoa
 )
-@IF %~1 NEQ 0 ( IF %~1 NEQ 1 (
-@    GOTO:EOC
+@if %~1 neq 0 ( if %~1 neq 1 (
+@    goto :eoa
 ))
-@REM %~1 should be 0 or 1
-@SET logfile=%~n0_LOG.txt
-@REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v shutdownwithoutlogon /t reg_dword /d %~1 /f
-@REM >> "%logfile%" 2>> "%logfile%"
-:EOC
-@REM @ECHO/
-@REM @ECHO/Press any key to exit . . .
-@REM @PAUSE >nul 2>nul
-@GOTO:EOF
+@set logfile=%~n0_LOG.txt
+@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v shutdownwithoutlogon /t reg_dword /d %~1 /f
+:eoa
+@goto :eof
