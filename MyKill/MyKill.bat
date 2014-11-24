7bef5447efb38f291f0e1f77a7e242d8f32c898b
mantislin <mntslin@gmail.com>
2014-11-25 01:55:23 +0800

    Revert "formatting code"
    This reverts commit c63a17388d089a5981ecae496ca91029f81dd7bb.

--- MyKill/MyKill.bat
+++ MyKill/MyKill.bat
@@ -48,7 +48,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET "TARGET="
 :SOF
-IF "%~1" EQU "" goto :eoa
+IF "%~1" EQU "" GOTO :ENDOF
 echo %~1 | findstr /C:"." >nul 2>nul
 IF %errorlevel% EQU 0 (
     IF "%TARGET%" EQU "" (
@@ -69,34 +69,36 @@ IF %errorlevel% EQU 0 (
         )
     )
 )
-
+REM 遍历处理所有参数
 IF "%~2" NEQ "" (
     shift/1
-    goto :SOF
+    GOTO :SOF
 )
 
+REM erl：变量，用于记录ErrorLevel
 SET "erl="
 call :DOKILL %TARGET%
 
+REM 如果没有错误则延迟0秒关闭，若出现错误延迟2秒关闭窗口
 IF %erl% LEQ 1 (
     SET "dly=0"
 ) ELSE (
     SET "dly=2"
 )
 
-:eoa
+:ENDOF
 IF "%dly%" EQU "" SET "dly=3"
 echo/
-echo Close in %dly% second(s)...
+echo %dly%秒后关闭窗口……
 call Delay.vbs %dly%
 ENDLOCAL
 exit/b
 
 :DOKILL
     SET "order=taskkill "
-    REM enumelate all arguments passed in
+    REM 遍历处理所有传入参数
     :SO1
-    IF "%~1" EQU "" goto :EOF
+    IF "%~1" EQU "" GOTO :EOF
     SET "order=!order!/im %~1 "
     IF "%~2" NEQ "" (
         shift/1
