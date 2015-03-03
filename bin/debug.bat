a6c9fcf27f94e9e878d03212ba1eb40def82e260
mantislin <mntslin@hotmail.com>
2015-03-04 04:49:04 +0800

    Nothing, just the debug file

--- bin/debug.bat
+++ bin/debug.bat
@@ -1,4 +1,2 @@
-@mymklink /s /j /r "Link" "Target"
-rem rem @set "link=abc_symlinkd"
-rem rem @call getType "theType" "%link%"
-rem rem @echo/type=%theType%
+@adminrun.bat "D:\uData\documents\Laboratory\AutoHotKey\mts.ahk"
+pause
